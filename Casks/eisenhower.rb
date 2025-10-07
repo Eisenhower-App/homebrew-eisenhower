61a97525d19fff232bad8e13e541f0ce90a1fc2a
Sidney de Koning <sidney@funky-monkey.nl>
2025-10-07 10:11:50 +0200

    Add Eisenhower cask formula v1.0.22

---
+++ Casks/eisenhower.rb
@@ -0,0 +1,23 @@
+cask "eisenhower" do
+  version "1.0.22"
+  sha256 "fb91cb9c0de8d55c83928b71b7054214ede701677c3f49003b7d66793e3ed7ec"
+
+  url "https://github.com/Eisenhower-App/eisenhower-releases/releases/download/v#{version}/Eisenhower-v#{version}-Installer.dmg"
+  name "Eisenhower"
+  desc "Task management app using the Eisenhower Matrix"
+  homepage "https://github.com/Eisenhower-App/eisenhower-releases"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Eisenhower.app"
+
+  zap trash: [
+    "~/Library/Application Support/Eisenhower",
+    "~/Library/Caches/com.sidneydekoning.Eisenhower",
+    "~/Library/Preferences/com.sidneydekoning.Eisenhower.plist",
+    "~/Library/Saved Application State/com.sidneydekoning.Eisenhower.savedState",
+  ]
+end
